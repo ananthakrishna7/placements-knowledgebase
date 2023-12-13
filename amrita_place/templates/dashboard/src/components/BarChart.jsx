06ca6cd8e1cd256a15fce0d8b1b560ae4bcc8145
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-12-13 09:19:25 +0000

    Comment mock data

--- amrita_place/templates/dashboard/src/components/BarChart.jsx
+++ amrita_place/templates/dashboard/src/components/BarChart.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@mui/material";
 import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
-import { mockBarData as data1 } from "../data/mockData";
+// import { mockBarData as data1 } from "../data/mockData";
 import { useEffect, useState } from "react";
 const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
