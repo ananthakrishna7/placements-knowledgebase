550c45c1d17b8dd6f3d3750ce30019c784199bc2
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-12-14 15:30:52 +0000

    Change uri

--- amrita_place/__init__.py
+++ amrita_place/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
 	# create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-            SECRET_KEY='dev',) # if necessary  SQLALCHEMY_DATABASE_URI='mysql://root:1Alakazam2anda@localhost/project'
+            SECRET_KEY='dev',) # if necessary  SQLALCHEMY_DATABASE_URI='mysql://root:pass@localhost/project'
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
