264f24fddd600015bcb74c5fc8a52f7e6a6ae9d6
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-12-15 11:56:50 +0000

    Revert "Backend integration for companies."
    This reverts commit c3c64644c250ce929b9425b09ecdfa6fd3aac3e6.

--- amrita_place/database.py
+++ amrita_place/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
-engine = create_engine('mysql+pymysql://root:1Alakazam2anda@localhost/placements') # may have to install dependencies
+engine = create_engine('mysql+pymysql://root:github@localhost/placements') # may have to install dependencies
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
